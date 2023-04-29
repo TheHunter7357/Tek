c8b12c384ca5734cb1d3dd2e081aca8d6a388347
TheHunter7357 <55022262+TheHunter7357@users.noreply.github.com>
2023-04-29 16:03:17 +0200

    first commit

---
+++ TekEditor/GameProject/NewProject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekEditor.GameProject
+{
+    public class ProjectTemplate
+    {
+        public string ProjectType { get; set; }
+        public string ProjectFile { get; set; }
+
+    }
+    class NewProject : ViewModelBase
+    {
+        private string _name = "NewProject";
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\TekProject\";
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+
+    }
+}
